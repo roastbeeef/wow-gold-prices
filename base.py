f838d5407633451db7e257a7fcd7b776967b2b41
roastbeeef <mattwilsn@gmail.com>
2021-06-07 10:52:55 +0100

    created function to write data

--- base.py
+++ base.py
@@ -1,21 +1,20 @@
-from functions import get_data
-from statistics import mean
+from functions import get_data, write_data
 from datetime import datetime
 
 # globals
 BASE_URL = 'https://www.g2g.com/wow-classic-tbc/gold-29076-29077?faction=41397&server=41092'
 RECOMMENDED = '&sorting=recommended'
 CHEAPEST = '&sorting=lowest_price'
+DATA_DIRECTORY = 'data'
+RECOMMENDED_FILE = 'datalog_recommended.txt'
+CHEAPEST_FILE = 'datalog_cheap.txt'
+
 
 now = datetime.now()
 dt_string = now.strftime("%B %d %Y %H:%M:%S")
 
 if __name__ == "__main__":
-    rec = get_data(BASE_URL, RECOMMENDED)
-    cheap = get_data(BASE_URL, CHEAPEST)
-
-    r = open('data/datalog_recommended.txt', 'a')
-    r.write(dt_string + "," + str(mean(rec)) + '\n')
-
-    c = open('data/datalog_cheap.txt', 'a')
-    c.write(dt_string + "," + str(mean(cheap)) + '\n')
+    iterations = zip([RECOMMENDED, CHEAPEST], [RECOMMENDED_FILE, CHEAPEST_FILE])
+    for dataset, write_file in iterations:
+        _ = get_data(BASE_URL, dataset)
+        write_data(DATA_DIRECTORY, write_file, _, dt_string)
--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
+from statistics import mean
 import requests
 
+
 def get_data(url, sort):
+    """extract data from website, return list of outcomes"""
     page = requests.get(url+sort)
 
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -14,3 +17,9 @@ def get_data(url, sort):
             'span', class_='products__exch-rate').span.text.strip()))
 
     return datapoints
+
+
+def write_data(directory, file, data, date_string):
+    """write data to text file"""
+    file = open(f"{directory}/{file}", "a")
+    file.write(date_string + "," + str(mean(data)) + "\n")
