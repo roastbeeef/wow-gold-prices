2c304f7726f2edfd36e6d43d549a06735039dfcd
unknown <msw@skroom.org>
2021-06-10 19:04:46 +0200

    add dynamodb functionality & split server/price update by args

--- base.py
+++ base.py
@@ -1,25 +1,24 @@
-from datetime import datetime
+import argparse
+
 
 from functions import (
     get_data,
-    get_server_list
+    get_server_list,
+    update_prices,
+    update_servers
 )
 
-from utils import (
-    write_data,
-    write_data_as_json
-)
 
 import vars
 
-now = datetime.now()
-dt_string = now.strftime("%B %d %Y %H:%M:%S")
-
 if __name__ == "__main__":
-    server_list = get_server_list(vars.BASE_URL_)
-    write_data_as_json(vars.DATA_DIRECTORY, vars.SERVER_LIST_FILE, server_list)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-a", "--action", help="Update server list or get latest prices", type=str, choices=['prices', 'servers'])
 
-    iterations = zip([vars.RECOMMENDED, vars.CHEAPEST], [vars.RECOMMENDED_FILE, vars.CHEAPEST_FILE])
-    for dataset, write_file in iterations:
-        _ = get_data(vars.BASE_URL, dataset, vars.TEMP_SERVER_NAME)
-        write_data(vars.DATA_DIRECTORY, write_file, _, dt_string)
+    args = parser.parse_args()
+
+    if(args.action == "prices"):
+        update_prices()
+    if(args.action == "servers"):
+        update_servers()
